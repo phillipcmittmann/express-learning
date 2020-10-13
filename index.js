53ac34c3b8a0b373f5f5bafdd6083dc3243f3ebb
Phillip <phillip.mittmann@gmail.com>
2020-10-13 20:03:28 -0300

    Adicionada segunda rota

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", function(request, response) {
+    response.send("Teste de resposta.");
+});
+
+app.get("/blog", function(request, response) {
+    response.send("Teste de segunda rota.");
+});
+
 app.listen(4000, function(error) {
     if(error) {
         console.log("Ocorreu um erro.");
