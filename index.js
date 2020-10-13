c5a10af6a3f085b34834135b5385c39d8c4e5d7b
Phillip <phillip.mittmann@gmail.com>
2020-10-13 20:26:55 -0300

    Rotas com parametros

--- index.js
+++ index.js
@@ -6,8 +6,18 @@ app.get("/", function(request, response) {
     response.send("Teste de resposta.");
 });
 
-app.get("/blog", function(request, response) {
-    response.send("Teste de segunda rota.");
+app.get("/blog/:artigo?", function(request, response) {
+    var artigo = request.params.artigo;
+
+    if (artigo) {
+        response.send(`<h2>Artigo: ${artigo}.</h2>`)
+    } else {
+        response.send("Teste de segunda rota.");
+    }
+});
+
+app.get("/ola/:nome/:empresa", function(request, response) {
+    response.send(`<h1>Oi ${request.params.nome} da empresa.</h1>`);
 });
 
 app.listen(4000, function(error) {
@@ -15,5 +25,5 @@ app.listen(4000, function(error) {
         console.log("Ocorreu um erro.");
     } else {
         console.log("Servidor iniciado com sucesso.");
-    };
-})
+    }
+});
